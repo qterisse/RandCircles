7d2809f6bd2c534df58506a435e4aff181ff381e
mojosama <142836386+mojosama@users.noreply.github.com>
2024-01-12 17:19:36 +0100

    ft_rand(int min, int max)

--- utils/math_utils.c
+++ utils/math_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   math_utils.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: quteriss <quteriss@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hlopez <hlopez@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 15:32:33 by quteriss          #+#    #+#             */
-/*   Updated: 2024/01/12 16:04:42 by quteriss         ###   ########.fr       */
+/*   Updated: 2024/01/12 17:09:16 by hlopez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,4 +31,15 @@ int min(int n1, int n2)
 	if (n1 < n2)
 		return (n1);
 	return (n2);
+}
+
+int	ft_rand(int min, int max)
+{
+	double	n;
+	n = rand();
+	n /= 2147483647;
+	n *= max;
+	if (n < min)
+		n += min;
+	return ((int)n);
 }
